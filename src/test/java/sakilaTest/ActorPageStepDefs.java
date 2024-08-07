768d54806ac26b646c60adbf0c92cd455ecbd5bb
Arda Ordu <ardaordu03@gmail.com>
2024-08-07 12:13:26 +0100

    Complete Actor Page feature tests.

--- src/test/java/sakilaTest/ActorPageStepDefs.java
+++ src/test/java/sakilaTest/ActorPageStepDefs.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -41,9 +42,37 @@ public class ActorPageStepDefs {
         driver.get("http://localhost:5173/");
     }
 
+    @Given("the user is on the actor page")
+    public void theUserIsOnTheActorPage() {
+        driver.get("http://localhost:5173/actors");
+    }
+
     @When("the user clicks on the Actors link")
     public void theUserClicksOnTheActorsLink() {
         driver.findElement(By.linkText("Actors")).click();
+    }
+
+    @When("the user clicks on an actor")
+    public void theUserClicksOnAnActor() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".actor-list")));
+        WebElement actorLink = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//li/a[text()[contains(.,'PENELOPE')]][text()[contains(.,'GUINESS')]]")
+        ));
+        actorLink.click();
+    }
+
+    @When("the user clicks on the search bar")
+    public void theUserClicksOnTheSearchBar() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".search-bar")));
+        WebElement searchInput = driver.findElement(By.cssSelector(".search-bar"));
+        searchInput.click();
+    }
+
+    @When("the user clicks on the create actor button")
+    public void theUserClicksOnTheCreateActorButton() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".create-button")));
+        WebElement createActorButton = driver.findElement(By.cssSelector(".create-button"));
+        createActorButton.click();
     }
 
     @Then("the user should be taken to the Actor page")
@@ -52,10 +81,93 @@ public class ActorPageStepDefs {
         assertEquals("http://localhost:5173/actors", currentUrl);
     }
 
+    @Then("the user should be taken to the actor details page")
+    public void theUserShouldBeTakenToTheActorDetailsPage() {
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(currentUrl.contains("/actors/"));
+    }
+
+    @Then("the correct actor should be displayed")
+    public void theCorrectActorShouldBeDisplayed() {
+        WebElement actorLink = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//li/a[text()[contains(.,'ED')]][text()[contains(.,'CHASE')]]")
+        ));
+        actorLink.click();
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("/actors/"), "Navigated to the actor's detail page");
+
+    }
+
+    @Then("the actor should be added to the actor list")
+    public void theActorShouldBeAddedToTheActorList() {
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert.accept();
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("/actors"), "Can create an actor");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".search-bar")));
+        WebElement searchInput = driver.findElement(By.cssSelector(".search-bar"));
+        searchInput.click();
+        searchInput.sendKeys("John Doe");
+        WebElement actorLink = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//li/a[text()[contains(., 'JOHN')]][text()[contains(., 'DOE')]]")
+        ));
+        actorLink.click();
+        currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("/actors"), "Can search for created actor");
+    }
+
+    @Then("the form should not be submitted")
+    public void theFormShouldNotBeSubmitted() {
+        String currentUrl = driver.getCurrentUrl();
+        String expectedUrl = "http://localhost:5173/actors/create";
+        Assert.assertEquals(currentUrl, expectedUrl, "The URL is correct");
+    }
+
     @And("the user should see a list of actors")
     public void theUserShouldSeeAListOfActors() {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".actor-list")));
         List<WebElement> actorList = driver.findElements(By.cssSelector(".actor-list li"));
         Assert.assertFalse(actorList.isEmpty(), "The actor list is not empty");
     }
+
+
+    @And("the user should see the actor details")
+    public void theUserShouldSeeTheActorDetails() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".film-list")));
+        List<WebElement> actorDetails = driver.findElements(By.cssSelector(".film-list li"));
+        Assert.assertFalse(actorDetails.isEmpty(), "The actor details are not empty");
+    }
+
+
+    @And("types the name of an actor")
+    public void typesTheNameOfAnActor() {
+        WebElement searchInput = driver.findElement(By.cssSelector(".search-bar"));
+        searchInput.sendKeys("Ed Chase");
+    }
+
+
+    @And("the user enters valid actor details")
+    public void theUserEntersValidActorDetails() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".create-button")));
+        WebElement createActorButton = driver.findElement(By.cssSelector(".create-button"));
+        createActorButton.click();
+        driver.findElement(By.id("firstName")).sendKeys("John");
+        driver.findElement(By.id("lastName")).sendKeys("Doe");
+    }
+
+    @And("the user submits the actor form")
+    public void theUserSubmitsTheActorForm() {
+        driver.findElement(By.cssSelector(".create-button")).click();
+    }
+
+
+
+    @And("the user leaves the actor details blank")
+    public void theUserLeavesTheActorDetailsBlank() {
+        WebElement firstNameInput = driver.findElement(By.id("firstName"));
+        firstNameInput.clear();
+        WebElement lastNameInput = driver.findElement(By.id("lastName"));
+        lastNameInput.clear();
+    }
+
 }
--- src/test/resources/sakilaTest/ActorPage.feature
+++ src/test/resources/sakilaTest/ActorPage.feature
@@ -6,9 +6,28 @@ Feature: Actor Page
     Then the user should be taken to the Actor page
     And the user should see a list of actors
 
-
   Scenario: See actor details
     Given the user is on the actor page
     When the user clicks on an actor
     Then the user should be taken to the actor details page
     And the user should see the actor details
+
+  Scenario: Search for an actor
+    Given the user is on the actor page
+    When the user clicks on the search bar
+    And types the name of an actor
+    Then the correct actor should be displayed
+
+  Scenario: Successfully create a new actor
+    Given the user is on the actor page
+    When the user clicks on the create actor button
+    And the user enters valid actor details
+    And the user submits the actor form
+    Then the actor should be added to the actor list
+
+  Scenario: Attempt to create actor with empty fields
+    Given the user is on the actor page
+    When the user clicks on the create actor button
+    And the user leaves the actor details blank
+    And the user submits the actor form
+    Then the form should not be submitted
